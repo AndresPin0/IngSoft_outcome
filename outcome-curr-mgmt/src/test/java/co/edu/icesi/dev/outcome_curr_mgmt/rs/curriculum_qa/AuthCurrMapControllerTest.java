f1b766868c515008551770cd2ff5e7dfbf2d8a23
Andrés Pino <84475698+AndresPin0@users.noreply.github.com>
2024-11-21 19:24:16 -0500

    Update AuthCurrMapControllerTest.java

--- outcome-curr-mgmt/src/test/java/co/edu/icesi/dev/outcome_curr_mgmt/rs/curriculum_qa/AuthCurrMapControllerTest.java
+++ outcome-curr-mgmt/src/test/java/co/edu/icesi/dev/outcome_curr_mgmt/rs/curriculum_qa/AuthCurrMapControllerTest.java
@@ -1,110 +1,79 @@
-package co.edu.icesi.dev.outcome_curr_mgmt.rs.curriculum_qa;
+package co.edu.icesi.dev.outcome_curr_mgmt.testing.system.rs.curriculum_qa;
 
-import co.edu.icesi.dev.outcome_curr.mgmt.model.curriculum_qa.MatrixDTO;
-import co.edu.icesi.dev.outcome_curr.mgmt.rs.curriculum_qa.AuthCurrMapController;
-import co.edu.icesi.dev.outcome_curr_mgmt.mapper.curriculum_qa.CurrMapMapper;
-import co.edu.icesi.dev.outcome_curr_mgmt.model.entity.curriculum_definition.Course;
-import co.edu.icesi.dev.outcome_curr_mgmt.model.entity.curriculum_qa.StudOutcome;
-import co.edu.icesi.dev.outcome_curr_mgmt.service.curriculum_definition.CourseService;
-import co.edu.icesi.dev.outcome_curr_mgmt.service.curriculum_qa.CurrMapService;
-import co.edu.icesi.dev.outcome_curr_mgmt.service.curriculum_qa.StudOutcomeService;
-import org.junit.jupiter.api.BeforeEach;
+import co.edu.icesi.dev.outcome_curr.mgmt.model.stdoutdto.curriculum_qa.AssmtGenPlanOutDTO;
+import co.edu.icesi.dev.outcome_curr_mgmt.rs.curriculum_qa.AuthAssessmentGenPlanControllerImpl;
+import co.edu.icesi.dev.outcome_curr_mgmt.testing.system.rs.util.BaseSmokeIT;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class AuthCurrMapControllerTest {
+@SpringBootTest(classes = {AuthAssessmentGenPlanControllerImpl.class})
+@RequiredArgsConstructor
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AssessmentGenPlanControllerSmokeIT extends BaseSmokeIT {
 
-    public static final String TEST_KEY = "test";
-    public static final String TEST_VALUE = "test";
+    @Value("${test.server.url}")
+    private String server;
 
-    private StudOutcomeService studOutcomeService;
-    private CourseService courseService;
-    private CurrMapService currMapService;
-    private CurrMapMapper currMapMapper;
-    private AuthCurrMapController authCurrMapController;
+    private static String testUserJWTToken;
 
-    @BeforeEach
-    public void init() {
-        studOutcomeService = mock(StudOutcomeService.class);
-        courseService = mock(CourseService.class);
-        currMapService = mock(CurrMapService.class);
-        currMapMapper = mock(CurrMapMapper.class);
-        authCurrMapController = new AuthCurrMapControllerImpl(studOutcomeService, courseService, currMapService);
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @BeforeAll
+    void init() {
+        testUserJWTToken = getTestUserJWTToken("OutCurrTestUser", "123456", server);
     }
 
-    // Returns a MatrixDTO object with expected values when given valid facultyId, acadProgId, and acadProgCurrId, and no courses.
     @Test
-    void testGetMatrix() {
-        // Arrange
-        long facultyId = 1;
-        long acadProgId = 1;
-        long acadProgCurrId = 1;
+    void getAssessmentGenPlanList() {
+        HttpEntity<String> jwtEntity = new HttpEntity<>(getRequestHeaders());
 
-        List<StudOutcome> studentOutcomes = new ArrayList<>();
+        String url = server + "/outcurrapi/v1/assessment/genplan?facultyId=1&acadProgId=1";
+        ResponseEntity<List<AssmtGenPlanOutDTO>> response = testRestTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                jwtEntity,
+                new ParameterizedTypeReference<>() {});
 
-        List<Course> courses = new ArrayList<>();
-        MatrixDTO matrixDTO = MatrixDTO.builder()
-                .matrix(new ArrayList<>())
-                .build();
-
-        // Mock the service methods
-        when(studOutcomeService.getAllStudOutcomesByAcadProgCurrIdAndAcadProgIdAndFacultyId(acadProgCurrId, acadProgId, facultyId)).thenReturn(studentOutcomes);
-        when(courseService.findAllByAcadProgIdAndFacultyIdAndAcadProgCurrId(acadProgId, facultyId, acadProgCurrId)).thenReturn(courses);
-        when(currMapService.getMatrixDTO(acadProgCurrId, acadProgId, studentOutcomes, courses)).thenReturn(matrixDTO);
-
-        // Act
-        MatrixDTO result = authCurrMapController.getMatrix(facultyId, acadProgId, acadProgCurrId);
-
-        // Assert
-        assertTrue(result.matrix().isEmpty());
-        verify(studOutcomeService, times(1)).getAllStudOutcomesByAcadProgCurrIdAndAcadProgIdAndFacultyId(acadProgCurrId, acadProgId, facultyId);
-        verify(courseService, times(1)).findAllByAcadProgIdAndFacultyIdAndAcadProgCurrId(acadProgId, facultyId, acadProgCurrId);
-        verify(currMapMapper, never()).fromMapElementToValueDTO(any());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
     }
 
-    // Parameters passed to the controller method are valid.
     @Test
-    void testUpdateSuggestedCurrMapRequestStatus() {
-        // Arrange
-        long facultyId = 1;
-        long programId = 1;
-        long acadProgCurrId = 1;
-        long prevCurrMapId = 1;
-        long successorCurrMapId = 2;
-        String destinationState = "APPROVED";
+    void getAssessmentGenPlanById() {
+        HttpEntity<String> jwtEntity = new HttpEntity<>(getRequestHeaders());
 
-        doNothing().when(currMapService).updateSuggestedCurrMapRequestStatus(facultyId, programId, acadProgCurrId, prevCurrMapId, successorCurrMapId, destinationState);
+        String url = server + "/outcurrapi/v1/assessment/genplan/1?facultyId=1&acadProgId=1";
+        ResponseEntity<AssmtGenPlanOutDTO> response = testRestTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                jwtEntity,
+                new ParameterizedTypeReference<>() {});
 
-        // Act
-        authCurrMapController.updateSuggestedCurrMapRequestStatus(facultyId, programId, acadProgCurrId, prevCurrMapId, successorCurrMapId, destinationState);
-
-        // Assert
-        verify(currMapService, times(1)).updateSuggestedCurrMapRequestStatus(facultyId, programId, acadProgCurrId, prevCurrMapId, successorCurrMapId, destinationState);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
     }
 
-    // Destination state is invalid.
-    @Test
-    void testUpdateSuggestedCurrMapRequestStatusInvalidDestinationState() {
-        // Arrange
-        long facultyId = 1;
-        long programId = 1;
-        long acadProgCurrId = 1;
-        long prevCurrMapId = 1;
-        long successorCurrMapId = 2;
-        String destinationState = "INVALID_STATE";
-
-        doThrow(new IllegalArgumentException()).when(currMapService).updateSuggestedCurrMapRequestStatus(facultyId, programId, acadProgCurrId, prevCurrMapId, successorCurrMapId, destinationState);
-
-        // Act
-        assertThrows(IllegalArgumentException.class, () -> authCurrMapController.updateSuggestedCurrMapRequestStatus(facultyId, programId, acadProgCurrId, prevCurrMapId, successorCurrMapId, destinationState));
-
-        // Assert
-        verify(currMapService, times(1)).updateSuggestedCurrMapRequestStatus(facultyId, programId, acadProgCurrId, prevCurrMapId, successorCurrMapId, destinationState);
+    private HttpHeaders getRequestHeaders() {
+        HttpHeaders headers = getHeaders();
+        headers.set("Authorization", "Bearer " + testUserJWTToken);
+        return headers;
     }
 }
